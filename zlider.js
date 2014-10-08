378355c99c06c69e84c513a53221ffc6c7f87342
Aquietzero <zhaoyunhaosss@gmail.com>
2014-10-08 17:48:42 +0800

    add: first available version.

--- zlider.js
+++ zlider.js
@@ -9,15 +9,25 @@
   Zlider.prototype.init = function () {
     this.page = 0;
     this.max = this.getMax();
+
     this.width = window.innerWidth;
     this.height = window.innerHeight;
 
+    this.touching = false;
+
+    this.beginPosition = {
+      x: 0,
+      y: 0
+    }
+    this.touchingPosition = {
+      x: 0,
+      y: 0
+    };
+
+    this.direction = null;
+
     this.setStyles();
     this.bindEvents();
-
-    //setInterval(function () {
-    //  this.next();
-    //}.bind(this), 1000);
   }
 
   Zlider.prototype.setStyles = function () {
@@ -44,8 +54,58 @@
       }
     }.bind(this));
 
+    document.addEventListener('touchmove', function (e) {
+      e.preventDefault();
+
+      var y = e.touches[0].clientY;
+      var delta = y - this.touchingPosition.y;
+
+      if (!this.direction) {
+        this.direction = delta > 0 ? 'down' : 'up';
+      }
+
+      if (this.direction == 'up') {
+        this.moveNext(delta);
+      } else {
+        this.moveCurr(delta);
+      }
+
+      this.touchingPosition = {
+        x: e.touches[0].clientX,
+        y: e.touches[0].clientY
+      }
+    }.bind(this));
+
     document.addEventListener('touchstart', function (e) {
-      this.next();
+      this.touching = true;
+      this.beginPosition = {
+        x: e.touches[0].clientX,
+        y: e.touches[0].clientY
+      }
+      this.touchingPosition = {
+        x: e.touches[0].clientX,
+        y: e.touches[0].clientY
+      }
+    }.bind(this));
+
+    document.addEventListener('touchend', function (e) {
+      var delta = Math.abs(this.touchingPosition.y - this.beginPosition.y);
+      if (delta > this.height / 5) {
+        if (this.direction == 'up') {
+          this.next();
+        } else {
+          this.prev();
+        }
+      } else {
+        if (this.direction == 'up') {
+          this.resetNext();
+        } else {
+          this.resetCurr();
+        }
+      }
+
+      this.touching = false;
+      this.direction = null;
     }.bind(this));
   }
 
@@ -63,6 +123,8 @@
     var curr = this.sliders[this.page + 1];
     var next = this.sliders[this.page];
 
+    curr.style['transition-duration'] = '0.5s';
+    curr.style['-webkit-transition-duration'] = '0.5s';
     curr.style.top = this.height + 'px';
   }
 
@@ -76,7 +138,59 @@
     var curr = this.sliders[this.page - 1];
     var next = this.sliders[this.page];
 
+    next.style['transition-duration'] = '0.5s';
+    next.style['-webkit-transition-duration'] = '0.5s';
     next.style.top = 0;
+  }
+
+  Zlider.prototype.resetCurr = function () {
+    if (this.page == 0) {
+      return;
+    }
+
+    var curr = this.sliders[this.page];
+
+    curr.style['transition-duration'] = '0.5s';
+    curr.style['-webkit-transition-duration'] = '0.5s';
+    curr.style.top = '0px';
+  }
+
+  Zlider.prototype.resetNext = function () {
+    if (this.page == this.max) {
+      return;
+    }
+
+    var next = this.sliders[this.page + 1];
+
+    next.style['transition-duration'] = '0.5s';
+    next.style['-webkit-transition-duration'] = '0.5s';
+    next.style.top = this.height + 'px';
+  }
+
+  Zlider.prototype.moveCurr = function (delta) {
+    if (this.page == 0) {
+      return;
+    }
+
+    var curr = this.sliders[this.page];
+
+    var top = parseInt(curr.style.top.replace('px', ''));
+    curr.style['transition-duration'] = '0s';
+    curr.style['-webkit-transition-duration'] = '0s';
+    curr.style.top = top + delta + 'px';
+  }
+
+  Zlider.prototype.moveNext = function (delta) {
+    if (this.page == this.max) {
+      return;
+    }
+
+    var next = this.sliders[this.page + 1];
+
+    var top = parseInt(next.style.top.replace('px', ''));
+    next.style['transition-duration'] = '0s';
+    next.style['-webkit-transition-duration'] = '0s';
+    next.style.top = top + delta + 'px';
   }
 
   var slider = new Zlider('.zlider');
